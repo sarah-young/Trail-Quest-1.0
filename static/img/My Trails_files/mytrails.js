df20b15f5b404cc4ad81b304399e59629634b2cb
Sarah Dicker <sarah@sarahs-mbp.lan>
2018-03-07 17:23:22 -0800

    Adding more files

---
+++ static/img/My Trails_files/mytrails.js
@@ -0,0 +1,27 @@
+
+// Logic for submitting reviews on mytrails pages
+$('.trailcompleted').click(function() {
+
+  $.post('/submit_review', {'trail_id' : this.name,
+                            'review_text' : document.getElementById((this.name+'-text')).value},
+    function(result) {
+      console.log(result);
+    })
+    let trail_id = this.name;
+    $("#div-"+trail_id).removeClass("trek-details");
+    $("#div-"+trail_id).addClass("review-trails");
+    $("#"+trail_id+"-text").hide();
+});
+
+// Logic to remove trail from mytrails page
+$('.removetrail').click(function() {
+   console.log('banana');
+  $.post('/remove_trail', {'trail_id' : this.name },
+
+    function(result) {
+      console.log(result);
+    });
+    let trail_id = this.name;
+    $("#div-"+trail_id).remove();
+
+            });
